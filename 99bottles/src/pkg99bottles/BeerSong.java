d9912bd446c92bf22c3bc8b3f4d1a72cc690c22b
Eric <ecdolley@oakland.edu>
2016-02-03 23:48:54 -0500

    created logic for the 99 bottles function and added a call to the 99bottles function in the main method

--- 99bottles/src/pkg99bottles/BeerSong.java
+++ 99bottles/src/pkg99bottles/BeerSong.java
@@ -6,11 +6,23 @@ public class BeerSong {
 
 
     public static void main(String[] args) {
-
+        Ninety_Nine_Bottles_Of_Beer();
     }
     
     public static void Ninety_Nine_Bottles_Of_Beer() {
-
+        
+        int bottles = 99;
+        while (bottles>0){
+            System.out.println(bottles + " bottles of beer on the wall, " + bottles + " bottles of beer");
+            bottles--;
+            if (bottles ==0){
+            System.out.println("Take one down, pass it around, no more bottles of beer on the wall");
+            } else{
+            System.out.println("Take one down, pass it around, " + bottles + " bottles of beer on the wall");    
+            }
+            
         }
+    
+    }
     
 }
