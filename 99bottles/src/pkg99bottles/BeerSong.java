00bfd3c94e853dcfb404782e0cc101be39deb244
Eric <ecdolley@oakland.edu>
2016-02-03 23:41:39 -0500

    created an empty main function and an empty 99bottles function

---
+++ 99bottles/src/pkg99bottles/BeerSong.java
@@ -0,0 +1,16 @@
+
+package pkg99bottles;
+
+
+public class BeerSong {
+
+
+    public static void main(String[] args) {
+
+    }
+    
+    public static void Ninety_Nine_Bottles_Of_Beer() {
+
+        }
+    
+}
